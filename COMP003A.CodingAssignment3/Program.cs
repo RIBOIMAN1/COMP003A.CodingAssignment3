0d0e1f0e0bd4a62477e47078d695065d94d0e210
RIBOIMAN1 <rileybenson@my.whccd.edu>
2025-01-29 12:46:02 -0800

    Add project files.

---
+++ COMP003A.CodingAssignment3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace COMP003A.CodingAssignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
