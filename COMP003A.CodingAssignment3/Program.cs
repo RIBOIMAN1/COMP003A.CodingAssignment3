0271427e651c4e39070778e99a8d761d065e363c
RIBOIMAN1 <rileybenson@my.whccd.edu>
2025-01-30 13:08:41 -0800

    Added a starting expense amount of $0, a starting expense name of "None", a ReadLine command for monthly income, a while (true) loop, menu text, a ReadLine command for the selected choice, a switch statement for the selected choice, and corresponding output relating to the choice you selected. These changes apply to the file "Program.cs."

--- COMP003A.CodingAssignment3/Program.cs
+++ COMP003A.CodingAssignment3/Program.cs
@@ -9,14 +9,43 @@ namespace COMP003A.CodingAssignment3
     {
         static void Main(string[] args)
         {
+            double expenseAmount = 0;
+            string expenseName = "None";
+
             Console.WriteLine("Welcome to the Budget Management Tool!\n");
-            Console.Write("Enter your monthly income: \n");
-            Console.WriteLine("Menu:");
+            Console.Write("Enter your monthly income: ");
+            double monthlyIncome = double.Parse(Console.ReadLine());
+            while (true)
+            {
+            Console.WriteLine("\nMenu:");
             Console.WriteLine("1. Add an Expense");
             Console.WriteLine("2. View Expenses and Budget");
             Console.WriteLine("3. Remove an Expense");
             Console.WriteLine("4. Exit");
             Console.Write("Enter your choice: ");
+            int choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1: Console.Write("Enter the expense name: ");
+                expenseName = (Console.ReadLine());
+                Console.Write("Enter the expense amount: ");
+                expenseAmount = double.Parse(Console.ReadLine());
+                Console.WriteLine("Expense added successfully!");
+                    break;
+                case 2: Console.WriteLine("\nExpenses:");
+                Console.WriteLine($"- {expenseName}: {expenseAmount}");
+                Console.Write($"Total Expenses: {expenseAmount}");
+
+                Console.Write("");
+
+                    break;
+                case 3: Console.Write("");
+                    break;
+                case 4: Console.Write("");
+                Console.WriteLine("Goodbye!");
+                    return;
+            }
+            }
             Console.WriteLine("\n\nExpenses:");
             Console.WriteLine("- Rent: $");
             Console.WriteLine("Total Expenses: $");
