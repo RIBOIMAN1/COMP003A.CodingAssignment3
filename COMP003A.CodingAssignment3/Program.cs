5dab68139d38c4c078b7f6c56c2a570e72434405
RIBOIMAN1 <rileybenson@my.whccd.edu>
2025-01-29 16:04:53 -0800

    Added the monthly income entry point, the menu text, the 4 numbered bullet points, and the enter your choice dialogues to the file "Program.cs."

--- COMP003A.CodingAssignment3/Program.cs
+++ COMP003A.CodingAssignment3/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace COMP003A.CodingAssignment3
+﻿// Author: Riley Benson
+// Course: COMP-003A
+// Faculty: Jonathan Cruz
+// Purpose: Budget management application demonstrating control flow.
+
+namespace COMP003A.CodingAssignment3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to the Budget Management Tool!\n");
+            Console.Write("Enter your monthly income: \n");
+            Console.WriteLine("Menu:");
+            Console.WriteLine("1. Add an Expense");
+            Console.WriteLine("2. View Expenses and Budget");
+            Console.WriteLine("3. Remove an Expense");
+            Console.WriteLine("4. Exit");
+            Console.Write("Enter your choice: ");
         }
     }
 }
